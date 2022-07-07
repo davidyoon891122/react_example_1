9c7ac40a62a0aa85a2b9eaab8191f294d53f77eb
davidyoon <qqpo12@naver.com>
2022-07-07 22:52:53 +0900

    add set Text by isClick State

--- index.js
+++ index.js
@@ -6,26 +6,16 @@ function helloWorldButton() {
 
     // 비 구조화 할당
     const [isClick, setClickState] = React.useState(false) // 컴포넌트의 상태를 설정하는 메소드, 파라미터는 초기값
-    console.log(isClick) // false
-    setClickState(true) // setter 함수 
-    console.log(isClick) // true
-    
+    const text = isClick ? "Bye World!" : "Hello World!"
+
     return React.createElement(
         'button', 
         {onclick: () => {
 
         }},
-        'Hello World!'
+        text
     )
 }
 
 const rootContainer = document.getElementById('react-root')
 ReactDOM.render(React.createElement(helloWorldButton), rootContainer)
-
-
-// 비 구조화 할당 예제
-// function returnOneThreeArray() {
-//     return [1,3]
-// }
-
-// const [a, b] = returnOneThreeArray() // a == 1, b == 3
