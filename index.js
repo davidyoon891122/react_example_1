d0fdcbb59869aacb594a8f3a6c22200e890e46c5
davidyoon <qqpo12@naver.com>
2022-07-07 22:43:51 +0900

    Add button with React

---
+++ index.js
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+// 함수형 컴포넌트
+function helloWorldButton() {
+    return React.createElement(
+        'button', 
+        {onclick: () => {
+
+        }},
+        'Hello World!'
+    )
+}
+
+const rootContainer = document.getElementById('react-root')
+ReactDOM.render(React.createElement(helloWorldButton), rootContainer)
